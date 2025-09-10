82d0d92552edd7f793383bb0896f75c6282d147e
phoebus-84 <ennio@dyne.org>
2025-09-11 00:01:41 +0200

    fix: footer

--- webapp/src/lib/blocks/Footer.svelte
+++ webapp/src/lib/blocks/Footer.svelte
@@ -6,16 +6,15 @@
 
 <footer class="bg-helpo-dark text-gray-400 py-16">
 	<div class="max-w-5xl mx-auto">
-		<div class="row">
+		<div class="grid grid-cols-1 md:grid-cols-3 gap-8">
 			<!-- Column 1: Logo & Contacts -->
-			<div class="col-sm-6 col-lg-4 mb-8 lg:mb-0">
+			<div class="mb-8 lg:mb-0">
 				<div class="mb-6">
 					<Logo theme="dark" />
 				</div>
 				<div class="text-sm space-y-2">
-					<p>Elliott Ave, Parkville VIC 3052, Melbourne Canada</p>
-					<p>Phone: <a href="tel:+31859644725" class="no-underline hover:text-white">+31 85 964 47 25</a></p>
-					<p>Email: <a href="mailto:support@helpo.org" class="no-underline hover:text-white">support@helpo.org</a></p>
+					<p>Phone: <a href="tel:+31859644725" class="no-underline hover:text-white"></a></p>
+					<p>Email: <a href="mailto:support@helpo.org" class="no-underline hover:text-white"></a></p>
 				</div>
 				<ul class="flex space-x-8 mt-6 text-white text-lg">
 					<li><a href="#" class="hover:opacity-75"><i class="fa fa-facebook" /></a></li>
@@ -26,7 +25,7 @@
 			</div>
 
 			<!-- Column 2: Menu -->
-			<div class="col-sm-6 col-lg-4 col-xl-3 offset-xl-1 mb-8 lg:mb-0">
+			<div class="offset-xl-1 mb-8 lg:mb-0">
 				<h6 class="text-white text-xl font-bold mb-5">Menu & Links</h6>
 				<nav>
 					<ul class="grid grid-cols-2 gap-x-4 gap-y-2 text-sm">
@@ -43,7 +42,7 @@
 			</div>
 
 			<!-- Column 3: Newsletter -->
-			<div class="col-lg-4 col-xl-3 offset-xl-1">
+			<div class="offset-xl-1">
 				<h6 class="text-white text-xl font-bold mb-5">Newsletter</h6>
 				<form class="flex flex-col items-start">
 					<input
@@ -59,15 +58,9 @@
 		</div>
 
 		<!-- Copyright -->
-		<div class="row items-baseline mt-16 text-sm text-gray-600">
-			<div class="col-md-6 text-center md:text-left mb-4 md:mb-0">
-				<p>© 2024 Sanabel Template by You</p>
-			</div>
-			<div class="col-md-6 text-center md:text-right">
+		<div class="items-baseline mt-16 text-sm text-gray-600">
+			<div class="text-center md:text-right">
 				<a href="#" class="no-underline hover:text-white">Privacy Policy</a>
-				<span class="mx-2">|</span>
-				<a href="#" class="no-underline hover:text-white">Term and Condition</a>
-			</div>
 		</div>
 	</div>
 </footer>
