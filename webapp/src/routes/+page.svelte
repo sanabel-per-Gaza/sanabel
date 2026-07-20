fc3b4a84396f26320e2689f2249b110b8959a603
phoebus-84 <ennio@dyne.org>
2026-07-20 08:41:23 +0200

    fix: remove nodi section

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script>
     import Hero from '$lib/blocks/Hero.svelte';
     import AboutStats from '$lib/blocks/AboutStats.svelte';
-    import NodiTerritoriali from '$lib/blocks/NodiTerritoriali.svelte';
     import Attivita from '$lib/blocks/Attivita.svelte';
   </script>
   
   <main>
     <Hero />
     <AboutStats />
-    <NodiTerritoriali />
     <Attivita />
   </main>
